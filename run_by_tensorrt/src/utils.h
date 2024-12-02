fdaac21c1c99a9cfc4741f47993162b639bc58dc
meng <1792766874@qq.com>
2024-12-02 20:48:02 +0800

    delete InferDeleter

--- run_by_tensorrt/src/utils.h
+++ run_by_tensorrt/src/utils.h
@@ -6,17 +6,6 @@
 
 #include <iostream>
 
-
-// 自定义删除器
-struct InferDeleter {
-  template <typename T>
-  void operator()(T* obj) const {
-    if (obj) {
-      delete obj;
-    }
-  }
-};
-
 class Logger : public nvinfer1::ILogger {
  public:
   void log(Severity severity, const char* msg) noexcept override {
